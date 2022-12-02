695ac16143f72a72abdc1bc93ad70107770437bf
tharindurm <tharindurm@gmail.com>
2022-12-02 23:20:02 +0400

    remove unwanted lcd.clear() to make animation smooth

--- Arduino/LCD_Messages/Receiving/arrowAnimation/arrowAnimation.ino
+++ Arduino/LCD_Messages/Receiving/arrowAnimation/arrowAnimation.ino
@@ -1,12 +1,11 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
 }
 
@@ -18,33 +17,21 @@ void loop()
   lcd.setCursor(0, 0);
   lcd.print("Receiving");
   delay(500);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Receiving");
+
   lcd.setCursor(0, 1);
   lcd.print(">>>");
   delay(500);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Receiving");
   lcd.setCursor(0, 1);
   lcd.print(">>> >>>");
   delay(500);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Receiving");
   lcd.setCursor(0, 1);
   lcd.print(">>> >>> >>>");
   delay(500);
 
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Receiving");
   lcd.setCursor(0, 1);
   lcd.print(">>> >>> >>> >>>");
   delay(500);
+  
 }
