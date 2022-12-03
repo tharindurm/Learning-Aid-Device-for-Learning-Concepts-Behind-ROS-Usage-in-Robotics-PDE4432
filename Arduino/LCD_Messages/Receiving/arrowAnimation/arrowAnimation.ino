4131b38aa6e2c8c9dd6127cae5f6ab81385aa101
tharindurm <tharindurm@gmail.com>
2022-12-03 19:04:19 +0400

    ditched milis() for animation. Anim moved to separate function.

--- Arduino/LCD_Messages/Receiving/arrowAnimation/arrowAnimation.ino
+++ Arduino/LCD_Messages/Receiving/arrowAnimation/arrowAnimation.ino
@@ -3,35 +3,47 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+unsigned long currentTime = 0;
+unsigned long startTime = 0;
+
+boolean m1 = false;
+boolean m2 = false;
+boolean m3 = false;
+boolean m4 = false;
+
 void setup()
 {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
 }
 
-
-void loop()
-{
-
+void receivingDataAnimation() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Receiving");
-  delay(500);
+  delay(250);
 
   lcd.setCursor(0, 1);
   lcd.print(">>>");
-  delay(500);
+  delay(250);
 
   lcd.setCursor(0, 1);
   lcd.print(">>> >>>");
-  delay(500);
+  delay(250);
 
   lcd.setCursor(0, 1);
   lcd.print(">>> >>> >>>");
-  delay(500);
+  delay(250);
 
   lcd.setCursor(0, 1);
   lcd.print(">>> >>> >>> >>>");
-  delay(500);
-  
+  delay(250);
+
+}
+void loop()
+{
+  Serial.println("Got msg");
+  receivingDataAnimation();
+  Serial.println("Loop");
 }
