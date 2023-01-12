3d7f694e356c8d7a4a7dcf0e65fdd5ddf66bbdaa
tharindurm <tharindurm@gmail.com>
2023-01-12 21:43:29 +0400

    Arduino responds to topic data

--- Arduino/Combined/Code_Assembled/Code_Assembled.ino
+++ Arduino/Combined/Code_Assembled/Code_Assembled.ino
@@ -1,5 +1,23 @@
 #include <Wire.h> //LCD
 #include <LiquidCrystal_I2C.h> //LCD
+#include <ros.h> //rosserial
+#include <std_msgs/UInt32.h> //rosserial
+#include <std_msgs/String.h> //rosserial
+#include <Servo.h>
+
+void receivingDataAnimation(std_msgs::String& data);
+void updateTaskNumber(std_msgs::UInt32& data);
+void servoSweep();
+
+ros::NodeHandle nh;
+
+//Topic to receive task number
+ros::Subscriber<std_msgs::UInt32> task("taskTopic", &updateTaskNumber );
+
+//Topic to receive msgs to be displayed during tasks
+ros::Subscriber<std_msgs::String> lcd_message_number("lcd_msg", &receivingDataAnimation);
+
+
 
 //LCD screen instance
 LiquidCrystal_I2C LCD(0x27, 16, 2);
@@ -9,6 +27,8 @@ boolean deviceBusy  = false;
 
 //This variable holds the task number to be executed. 0 is no tasks
 int currentTask = 0;
+
+
 
 //Wheel PID controller variables
 bool motor_running = false;
@@ -24,11 +44,31 @@ int ki = 1;
 int kd = 1;
 
 
+//Servo motor variables
+Servo myservo;
+int pos = 0; 
+
+
+
+
+
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
+  //Initializing LCS\D
   LCD.init();
   LCD.backlight();
+
+  //Initializing Servo
+  myservo.attach(10);
+  
+  pinMode(13, OUTPUT);
+  
+  //Initializing ROS Node Handle
+  nh.initNode();
+  nh.subscribe(lcd_message);
+  nh.subscribe(task);
 
   //Standby LCD message
   LCD.clear();
@@ -43,13 +83,16 @@ void loop() {
   if (!deviceBusy) {
     deviceReadyMessage();
   }
-
+  
+  readSerialMsg();
+  
   if (currentTask == 1) {
-
+    digitalWrite(13,LOW);
   }
 
   if (currentTask == 2) {
-
+    digitalWrite(13,HIGH);
+    servoSweep();
   }
 
   if (currentTask == 3) {
@@ -60,7 +103,11 @@ void loop() {
 
   }
 
+  
+  nh.spinOnce();
+  delay(50);
 }
+
 
 void deviceReadyMessage() {
   LCD.clear();
@@ -69,6 +116,45 @@ void deviceReadyMessage() {
   LCD.setCursor(0, 1);
   LCD.print("Waiting for Task");
   delay(500);
+}
+
+void updateTaskNumber(std_msgs::UInt32& data){
+  currentTask = data.data;  
+
+}
+
+void servoSweep() {
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);              
+    delay(15);                        
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);             
+    delay(15);
+  }
+}
+
+void receivingDataAnimation(std_msgs::String& data) {
+
+  analogWrite(9,200);
+  delay(50);
+  analogWrite(9,0);
+  
+  LCD.clear();  
+  LCD.setCursor(0, 0);
+  LCD.print("Topic: /lcd_msg");
+  LCD.setCursor(0, 1);
+  LCD.print("Received: ");
+  LCD.setCursor(11, 1);
+  LCD.print(data.data);
+  delay(250);
+  
+}
+
+
+
+void readSerialMsg(){
+  
 }
 
 void pid() {
